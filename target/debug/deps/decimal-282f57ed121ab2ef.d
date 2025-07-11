b8b4361970647818f297a9f233974b62a083690e
VyachKa <106316728+Buff2out@users.noreply.github.com>
2025-07-12 00:46:35 +0300

    Delete target directory

--- target/debug/deps/decimal-282f57ed121ab2ef.d
+++
@@ -1,5 +0,0 @@
-/home/wave/src/me/rust/decimal/target/debug/deps/decimal-282f57ed121ab2ef.d: src/main.rs
-
-/home/wave/src/me/rust/decimal/target/debug/deps/libdecimal-282f57ed121ab2ef.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/decimal-7021cd43b9f75f3a.d
+++
@@ -1,5 +0,0 @@
-/home/wave/src/me/rust/decimal/target/debug/deps/decimal-7021cd43b9f75f3a.d: src/main.rs
-
-/home/wave/src/me/rust/decimal/target/debug/deps/libdecimal-7021cd43b9f75f3a.rmeta: src/main.rs
-
-src/main.rs:
