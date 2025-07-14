9b64fdc13618386be79025fecfc5601384039912
rorikstr <slava.070299@gmail.com>
2025-07-14 17:50:42 +0300

    extend

--- src/decimal.rs
+++ src/decimal.rs
@@ -16,6 +16,21 @@ impl Dec {
     pub fn get_sign(&self) -> bool {
         ((1_u32 << 31) & self.bits[Self::METAINFO]) != 0
     }
+
+    // Получаем масштаб (scale)
+    pub fn get_scale(&self) -> u32 {
+        (self.bits[Self::METAINFO] >> 16) & 0xFF
+    }
+
+    // Получаем бит по позиции (0..=95)
+    pub fn get_bit(&self, pos: u32) -> bool {
+        let pos = pos.min(95); // Ограничиваем 95
+
+        let part = Self::BEGIN - (pos / 32) as usize; // Определяем, в каком bits[] находится бит
+        let local_pos = pos % 32;
+
+        (self.bits[part] >> local_pos) & 1 != 0
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -38,8 +53,23 @@ impl BigDec {
         self.bits[Self::BEGIN] == 0
     }
 
-    // Получаем знак большого числа (true — отрицательное)
+    // Получаем знак числа (true — отрицательное)
     pub fn get_sign(&self) -> bool {
         ((1_u32 << 31) & self.bits[Self::METAINFO]) != 0
+    }
+
+    // Получаем масштаб (scale)
+    pub fn get_scale(&self) -> u32 {
+        (self.bits[Self::METAINFO] >> 16) & 0xFF
+    }
+
+    // Получаем бит по позиции (0..=223)
+    pub fn get_bit(&self, pos: u32) -> bool {
+        let pos = pos.min(223); // Ограничиваем 223
+
+        let part = Self::BEGIN - (pos / 32) as usize; // Определяем, в каком bits[] находится бит
+        let local_pos = pos % 32;
+
+        (self.bits[part] >> local_pos) & 1 != 0
     }
 }
